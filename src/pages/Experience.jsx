04140052e793707d7247a89042f6e0db41eedc43
Roygebrayel <roy.gebrayel11@gmail.com>
2023-06-22 11:37:49 +0300

    added the Experience.jsx page

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import "react-vertical-timeline-component/style.min.css"
+import SchoolIcon from "@material-ui/icons/School"
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+
+      <VerticalTimeline lineColor="#3e497a">
+
+        <VerticalTimelineElement 
+        className='vertical-timeline-element--education' 
+        date='2010-2014' 
+        iconStyle={{ background: "#3e497a", color: "#fff" }} 
+        icon={<SchoolIcon />}>
+          <h3 className='vertical-time-line-element-title'> My Random high school </h3>
+
+        </VerticalTimelineElement>
+
+      </VerticalTimeline>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import GitHubIcon from "@material-ui/icons/GitHub";
 import EmailIcon from "@material-ui/icons/Email";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import "../style/Home.css"
 const Home = () => {
   return (
     <div className='home'>
       <div className="about">
         <h2> hi my name is Roy Gebrayel</h2>
         <div className="prompt"><p>a software dev with a passion for learning and create</p>
+        <a href="http://github.com/roygebrayel" target="_blank" rel="noopener noreferrer">
         <GitHubIcon/>
+        </a>
         <EmailIcon/>
         <LinkedInIcon/>
         </div>
