fb42aa6f62541f81f01f868dcd4a89fa55b8e1df
Roygebrayel <roy.gebrayel11@gmail.com>
2023-06-22 10:49:55 +0300

    added all the css files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
-
+import React from 'react';
+import GitHubIcon from "@material-ui/icons/GitHub";
+import EmailIcon from "@material-ui/icons/Email";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="about">
+        <h2> hi my name is Roy Gebrayel</h2>
+        <div className="prompt"><p>a software dev with a passion for learning and create</p>
+        <GitHubIcon/>
+        <EmailIcon/>
+        <LinkedInIcon/>
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills </h1>
+        <ol className='list'>
+          <li className='item'>
+            <h2> front-End</h2>
+            <span> ReactJS , Angular , Redux</span>
+          </li>
+
+          <li className='item'>
+            <h2> Backend </h2>
+            <span>NodeJS  </span>
+          </li>
+
+          <li className='item'>
+            <h2> languages </h2>
+            <span> C++ , Java </span>
+          </li>
+
+        </ol>
+      </div>
+    </div>
   )
 }
 
