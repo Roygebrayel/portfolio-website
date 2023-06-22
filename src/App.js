8eb03de65f567b43fd6562789b2d69aca47eee96
Roygebrayel <roy.gebrayel11@gmail.com>
2023-06-22 08:16:32 +0300

    replaced the project with yarn pkg manager

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter as router, routes ,route } from 'react-router-dom';
+import { BrowserRouter as router, routes, route } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
-import Experience from './components/Experience';
-import Project from './components/Project';
+import Home from './pages/Home';
+import Experience from './pages/Experience';
+import Project from './pages/Project';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
       <router>
+        <Navbar />
         <routes>
 
-<route path="/" element={<Home />}/>  
-<route path="/experience" element={<Experience />} />
-<route path="/projects" element={<Project/>} />
-      </routes>
+          <route path="/" element={<Home />} />
+          <route path="/experience" element={<Experience />} />
+          <route path="/projects" element={<Project />} />
+        </routes>
       </router>
-      
-     
+
+
     </div>
   );
 }
