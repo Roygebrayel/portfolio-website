69572bd10baad6092b1b3c29d7408404e25e85fe
access <roy.gebrayel11@gmail.com>
2023-12-03 23:29:36 +0200

    s

---
+++ express-backend-function/index.js
@@ -0,0 +1,21 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+
+// const app = express();
+// const port = 3000;
+
+// // Middleware to parse incoming JSON and form data
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // Serve static files (assuming your loginFailedTemplate is a static HTML file)
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+ // ckhfkdhfcosholsholshvshvosviosizh
+
+// // Start the Express server
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
